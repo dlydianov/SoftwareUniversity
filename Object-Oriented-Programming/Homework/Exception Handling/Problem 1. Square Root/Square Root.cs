9830b81b4389284e68d7b3ce9bb04312a8e0d277
dlydianov <d.lydianov@gmail.com>
2015-11-24 23:46:20 +0200

    OOP Homework

---
+++ Object-Oriented-Programming/Homework/Exception Handling/Problem 1. Square Root/Square Root.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1.Square_Root
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    throw new ArgumentException("Number cannot be negative.");
+                }
+                double a = Math.Sqrt(n);
+                Console.WriteLine("{0} square root is: {1}",n, a);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("GoodBye");
+            }
+        }
+    }
+}
---
+++ Object-Oriented-Programming/Homework/Exception Handling/Problem 2. Enter Numbers/EnterNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2.Enter_Numbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int a = 1;
+            int b = 100;
+            ReadNumber(a, b);
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                int temp = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    while (input < start || input > end)
+                    {
+                        Console.WriteLine("Please enter numbers between {0} and {1}", start, end);
+                        input = int.Parse(Console.ReadLine());
+                    }
+                    while (input <= temp)
+                    {
+                        Console.WriteLine("Please enter a bigger number.");
+                        input = int.Parse(Console.ReadLine());
+                    }
+                    temp = input;
+                    input = int.Parse(Console.ReadLine());
+                }
+                
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("This is not a number.");
+                return 
+            }
+        }
+    }
+}
